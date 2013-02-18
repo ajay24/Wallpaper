410d8490c8d69f111d6605e23e7e3a138591a7b5
Ajay Pal <ajay.pec24@gmail.com>
2013-02-18 14:48:34 +0530

    first commit
    display static name

---
+++ src/com/ajay/wallpaper/MyPoint.java
@@ -0,0 +1,13 @@
+package com.ajay.wallpaper;
+
+public class MyPoint {
+	String text;
+	int x;
+	int y;
+
+	public MyPoint(String text, int x, int y) {
+		this.text = text;
+		this.x = x;
+		this.y = y;
+	}
+}
---
+++ src/com/ajay/wallpaper/MyPreferencesActivity.java
@@ -0,0 +1,36 @@
+package com.ajay.wallpaper;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MyPreferencesActivity extends PreferenceActivity {
+
+   
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.prefs);
+
+        Preference circlePreference = getPreferenceScreen().findPreference("numberOfCircles");
+        
+        circlePreference.setOnPreferenceChangeListener(numberCheckListener);
+    }
+
+    Preference.OnPreferenceChangeListener numberCheckListener=new OnPreferenceChangeListener() {
+		
+		public boolean onPreferenceChange(Preference preference, Object newValue) {
+			 if (newValue != null && newValue.toString().length() > 0
+			          && newValue.toString().matches("\\d*")) {
+			        return true;
+			      }
+			      // If now create a message to the user
+			      Toast.makeText(MyPreferencesActivity.this, "Invalid Input", Toast.LENGTH_SHORT).show();
+			      return false;
+		}
+	};
+}
